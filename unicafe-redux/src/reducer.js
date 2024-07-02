92c4a27aa3482b08d642257b608723494e31a42b
vilequarter <daparady@gmail.com>
2024-07-02 11:11:37 +0300

    exercise 6.1

--- unicafe-redux/src/reducer.js
+++ unicafe-redux/src/reducer.js
@@ -7,14 +7,29 @@ const initialState = {
 const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
-    case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
+    case 'GOOD': {
+      const newState = {
+        ...state,
+        good: state.good + 1
+      }
+      return newState
+    }
+    case 'OK': {
+      const newState = {
+        ...state,
+        ok: state.ok + 1
+      }
+      return newState
+    }
+    case 'BAD': {
+      const newState = {
+        ...state,
+        bad: state.bad + 1
+      }
+      return newState
+    }
     case 'ZERO':
-      return state
+      return initialState
     default: return state
   }
   
